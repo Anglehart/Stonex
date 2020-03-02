de0133508b55ee68e680fb9147fd732b5ac8d203
Anglehart <planez@bk.ru>
2020-03-02 17:53:52 +0300

    feat: add photogallery

--- functions.js
+++ functions.js
@@ -6,3 +6,15 @@ function uhfCheck() {
     document.getElementById('price').innerHTML = '226 000 ₽'
   }
 }
+
+function allPhotos() {
+  if (document.getElementsByClassName('photogallery-inner')[0].style.width != "100%"){
+    document.getElementsByClassName('photogallery-inner')[0].style.width = "100%";
+    document.getElementsByClassName('photos')[0].style.margin = "auto";
+    document.getElementById('arrow').style.transform = "rotate(180deg)";
+  } else {
+    document.getElementsByClassName('photogallery-inner')[0].style.width = "55.5%";
+    document.getElementsByClassName('photos')[0].style.margin = "0";
+    document.getElementById('arrow').style.transform = "rotate(0deg)";
+  }
+}
