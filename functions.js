142e7321d69701f9716be0a4af8dbfd3cd0b3d34
Anglehart <planez@bk.ru>
2020-03-12 14:13:25 +0300

    feat: add features table

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 window.onload = uhfCheck()
 function uhfCheck() {
   if (document.getElementById('uhfCheck').checked) {
-    document.getElementById('price').innerHTML = '230 000 ₽'
+    document.getElementById('price').innerHTML = '276 000 ₽'
   } else {
     document.getElementById('price').innerHTML = '226 000 ₽'
   }
