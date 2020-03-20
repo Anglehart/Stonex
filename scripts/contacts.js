243e9108f7a0f61e318989ba01c419ba5a724845
Boris Kobzarev <planez@bk.ru>
2020-03-20 17:54:27 +0300

    feat: add info block

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,15 +1,19 @@
 function hideAll(){
-    document.getElementById('contact-map').querySelectorAll('div').forEach(item => {item.classList.add('hidden-map')});
+  Array.from(document.getElementsByClassName('map-area')).forEach(item => {item.classList.add('hidden-map')});
+  Array.from(document.getElementsByClassName('info-map')).forEach(item => {item.classList.add('hidden-info')});
 }
 function mskclick(){
   hideAll();
   document.getElementById('msk-map').classList.remove('hidden-map');
+  document.getElementById('msk-info').classList.remove('hidden-info');
 }
 function spbclick(){
   hideAll();
   document.getElementById('spb-map').classList.remove('hidden-map');
+  document.getElementById('spb-info').classList.remove('hidden-info');
 }
 function klgclick(){
   hideAll();
   document.getElementById('klg-map').classList.remove('hidden-map');
+  document.getElementById('klg-info').classList.remove('hidden-info');
 }
